3ac03f441f37f37ae5fec32778baa3897553c2ed
Luan Pham <luan@cinnamon.is>
2021-01-14 15:58:11 +0700

    Update README and setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 
 from setuptools import find_packages, setup
 
-version = "0.0.1"
+version = "0.0.2"
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 
@@ -10,7 +10,7 @@ cwd = os.path.dirname(os.path.abspath(__file__))
 def write_version_file():
     version_path = os.path.join(cwd, "rmn", "version.py")
     with open(version_path, "w") as f:
-        f.write("__version__ = '{}'\n".format(version))
+        f.write(f"__version__ = '{version}'\n")
 
 
 write_version_file()
@@ -19,6 +19,8 @@ setup(
     name="rmn",
     description="Facial Expression Recognition using Residual Masking Network",
     version=version,
+    author="Luan Pham",
+    author_email="phamquiluan@gmail.com",
     packages=find_packages(
         exclude=["docs", "tests", "configs", "env", "script", "trainers", "utils"]
     ),
