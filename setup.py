d49df9d0c47004fe7ae2ff27d7245fcdb6c5bd43
Luan Pham <phamquiluan@gmail.com>
2022-12-18 15:13:59 +1100

    Change homepage to url and bump version to 3.0.13

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ with open("README.md", encoding="utf-8") as f:
 setup(
     name="rmn",
     description="Facial Expression Recognition using Residual Masking Network",
-    homepage="https://github.dev/phamquiluan/ResidualMaskingNetwork",
+    url="https://github.dev/phamquiluan/ResidualMaskingNetwork",
     long_description=long_description,
     long_description_content_type="text/markdown",
     version=version,
